e3f8f3e2b64adacf98e836e6f26016906e308b52
xinwenqiang <xinwenqiang@bytedance.com>
2023-07-20 15:44:30 +0800

    feat(config): support specify eni quota of instance
    (cherry picked from commit bd01c8fd9c0221990d6249c457626d2fef5b1568)

--- pkg/daemon/eni.go
+++ pkg/daemon/eni.go
@@ -122,7 +122,7 @@ func generateENIPoolCfg(cfg *config.Config, limits helper.InstanceLimits) pool.C
 func newEniResourceManager(cfg *config.Config, subnet helper.SubnetManager, secManager helper.SecurityGroupManager, volcApi helper.VolcAPI, allocatedResource map[string]types.NetResourceAllocated, k8s k8s.Service) (*eniResourceManager, error) {
 	log.InfoS("Creating EniResourceManager")
 	m := &eniResourceManager{}
-	limit, err := helper.NewInstanceLimitManager(volcApi)
+	limit, err := helper.NewInstanceLimitManager(volcApi, cfg)
 	if err != nil {
 		return nil, err
 	}
