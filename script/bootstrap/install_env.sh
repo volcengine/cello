88639af28e25fa21fe901817aa0981165e43ff39
Quan Wei <quanwei.153@bytedance.com>
2024-07-15 21:05:11 +0800

    chore(compatibility): support kernel RHEL distribution with 4.18 kernel
    (cherry picked from commit 69be8774e46ceff9497422317da0f19501ef488b)

--- script/bootstrap/install_env.sh
+++ script/bootstrap/install_env.sh
@@ -34,14 +34,21 @@ init_node_bpf() {
 }
 
 # check kernel version & enable cilium
+# kernel version equal and above 5.0
 read KERNEL_MAJOR_VERSION KERNEL_MINOR_VERSION < <(uname -r | awk -F . '{print $1,$2}')
+if [ "$KERNEL_MAJOR_VERSION" -gt 4 ]; then
+    echo "Linux kernel >= 5.0, initializing node BPF"
+    init_node_bpf
 # kernel version equal and above 4.19
-if { [ "$KERNEL_MAJOR_VERSION" -eq 4 ] && [ "$KERNEL_MINOR_VERSION" -ge 19 ]; } ||
-    [ "$KERNEL_MAJOR_VERSION" -gt 4 ]; then
-    echo "Init node BPF"
+elif [ "$KERNEL_MAJOR_VERSION" -eq 4 ] && [ "$KERNEL_MINOR_VERSION" -ge 19 ]; then
+    echo "Linux kernel >= 4.19, initializing node BPF"
+    init_node_bpf
+# kernel version equal and above 4.18 for RHEL release
+elif  [ -f "/etc/redhat-release" ] && [ "$KERNEL_MAJOR_VERSION" -eq 4 ] && [ "$KERNEL_MINOR_VERSION" -ge 18 ];  then
+    echo "Linux kernel >= 4.18-RHEL, initializing node BPF"
     init_node_bpf
 else
-  echo "Linux kernel version <= 4.19, cant install cilium"
+  echo "Linux kernel version < 4.19 ( or 4.18 for RHEL kernel), cant install cilium"
   exit 1
 fi
 
